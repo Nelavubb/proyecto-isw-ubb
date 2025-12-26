187e84d542c8ad3df4ca7cbf4b5a944c4d900d43
rubenUBB <ruben.hernandez2201@alumnos.ubiobio.cl>
2025-12-26 16:21:32 -0300

    [UPDATE] Se conecto la pagina de el Banco de preguntas con el backend para mostrar, agregar y modificar las preguntas

--- backend/src/routes/indexRoutes.js
+++ backend/src/routes/indexRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import authRoutes from './authRoutes.js';
 import themeRoutes from './themeRoutes.js';
 import subjectRoutes from './subjectRoutes.js';
-import questionRoutes from './questionsRoutes.js';
+import questionRoutes from './questionRoutes.js';
 import usersRoutes from './usersRoutes.js';
 import historyRoutes from './historyRoutes.js';
 
@@ -17,4 +17,3 @@ router.use('/users', usersRoutes);
 router.use('/history', historyRoutes);
 
 export default router;
-
